a74ba841570611c8a1aa3f1a185c7bbca76a90ce
thukyaw11 <helloeleven968@gmail.com>
2020-10-16 22:14:36 +0630

    feat: loader on download

--- src/mixins/pdfDownloadMixin.js
+++ src/mixins/pdfDownloadMixin.js
@@ -1,5 +1,8 @@
 export default {
     methods: {
+      sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+},
         downloadFile(year, content,pdf) {
           if(year == null || content == null){
             this.$http({
@@ -9,33 +12,40 @@ export default {
 
             })   .then((response) => {
                 this.forceFileDownload(response, "ရွေးကောက်ပွဲအစီရင်ခံစာ");
-      
+
               })
               .catch((error) => console.log(error));
           }else{
-    
-          this.$http({
-            method: "get",
-            url: require(`@/assets/result-pdfs/${year}/${content}/${pdf.pdfValue}.pdf`),
-            responseType: "arraybuffer",
-          })
-            .then((response) => {
-              console.log(response);
-              this.forceFileDownload(response, pdf.pdfText);
-    
-            })
-            .catch((error) => console.log(error));
+            this.isLoading = true;
+              this.$http({
+                method: "get",
+                url: require(`@/assets/result-pdfs/${year}/${content}/${pdf.pdfValue}.pdf`),
+                responseType: "arraybuffer",
+              })
+                .then((response) => {
+                  if(response.status == 200){
+                    this.forceFileDownload(response, pdf.pdfText);
+                  }
+
+
+
+                })
+                .then(()=>this.isLoading = false)
+                .catch((error) => console.log(error));
+
         }
         },
-    
-        forceFileDownload(response, pdf) {
-         
-          const url = window.URL.createObjectURL(new Blob([response.data]));
-          const link = document.createElement("a");
-          link.href = url;
-          link.setAttribute("download", `${pdf}.pdf`);
-          document.body.appendChild(link);
-          link.click();
+
+        async forceFileDownload(response, pdf) {
+
+          const url =await window.URL.createObjectURL(new Blob([response.data]));
+          const link =await document.createElement("a");
+          link.href =await url;
+          await link.setAttribute("download", `${pdf}.pdf`);
+          await document.body.appendChild(link);
+          await link.click();
+
+
         },
       },
 }
--- src/views/electionResultsView.vue
+++ src/views/electionResultsView.vue
@@ -1,12 +1,12 @@
 <template>
-  <b-col
-    sm="12"
-    md="9"
-    xs="12"
-    lg="9"
-    style="font-family: 'Myanmar Sans Pro', sans-serif;"
-    class="content"
-  >
+
+  <b-col sm="12"
+         md="9"
+         xs="12"
+         lg="9"
+         style="font-family: 'Myanmar Sans Pro', sans-serif;"
+         class="content">
+    <loading :active.sync="isLoading" :is-full-page="fullPage" :color="color" :transition="transition" :loader="loader"></loading>
     <div class="knowledge_box">
       <h3>
         <span @click="$router.go(-1)" class="link">{{contentFinal.year}} / {{contentFinal.content}}</span>
@@ -18,73 +18,94 @@
         <div class="image_container">
           <img src="../assets/pdfdownload.jpg" alt="download" class="pdf_img">
         </div>
-        <div class="text_container"><p class="pdf_text">
-          {{pdf.pdfText}}</p></div>
+        <div class="text_container">
+          <p class="pdf_text">
+            {{pdf.pdfText}}</p>
+        </div>
       </div>
 
     </div>
   </b-col>
+
 </template>
 <script>
-import pdfDownloadMixin from '../mixins/pdfDownloadMixin'
-export default {
-  mixins: [pdfDownloadMixin],
-  computed: {
-    contentFinal() {
-      return this.$route.params;
+
+  import pdfDownloadMixin from '../mixins/pdfDownloadMixin'
+  import Loading from 'vue-loading-overlay'
+  import 'vue-loading-overlay/dist/vue-loading.css'
+  export default {
+    mixins: [pdfDownloadMixin],
+    components: {
+      Loading
     },
-  },
-};
+    data() {
+      return {
+        isLoading: false,
+        fullPage: true,
+        loader : 'dots',
+        color : '#2a78fb',
+        transition: 'fade'
+      }
+    },
+    computed: {
+      contentFinal() {
+        return this.$route.params
+      }
+    }
+  }
+
 </script>
 
 
 <style scoped>
-.pdf_text{
-  padding: 10px;
-}
-.pdf_img{
-  width: 100%;
-  height: 100%;
-}
-#pdf_image_container {
-  margin-top: 20px;
-  width: 100%;
-  height: 100px;
-  cursor: pointer;
-}
-.pdf_image {
-  width: 31%;
-  margin: 10px;
-  height: 80px;
-  float: left;
-}
-.image_container {
-  width: 30%;
-  height: 70px;
-  margin: 5px;
-  float: left;
-}
-.text_container {
-  width: 63%;
-  float: left;
-  height: 70px;
-  margin: 5px;
-}
 
-.link {
-  color: blue;
-}
-.link:hover {
-  text-decoration: underline;
-}
-
-@media screen and (max-width: 400px) {
-  #pdf_image_container{
-    height: 300px;
+  .pdf_text {
+    padding: 10px;
   }
-  .pdf_image{
+  .pdf_img {
     width: 100%;
-    margin: 10px 0px;
+    height: 100%;
   }
-}
+  #pdf_image_container {
+    margin-top: 20px;
+    width: 100%;
+    height: 100px;
+    cursor: pointer;
+  }
+  .pdf_image {
+    width: 31%;
+    margin: 10px;
+    height: 80px;
+    float: left;
+  }
+  .image_container {
+    width: 30%;
+    height: 70px;
+    margin: 5px;
+    float: left;
+  }
+  .text_container {
+    width: 63%;
+    float: left;
+    height: 70px;
+    margin: 5px;
+  }
+
+  .link {
+    color: blue;
+  }
+  .link:hover {
+    text-decoration: underline;
+  }
+
+  @media screen and (max-width: 400px) {
+    #pdf_image_container {
+      height: 300px;
+    }
+    .pdf_image {
+      width: 100%;
+      margin: 10px 0px;
+    }
+  }
+
 </style>
